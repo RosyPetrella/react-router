70cd485f78379ccb0f81ddd80dce02f9d9721fdd
RosyPetrella <petrellamr@gmail.com>
2025-04-01 15:06:13 +0200

    Implement basic structure for blog frontend with routing and homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+// Creiamo il frontend del nostro Blog e le sue pagine!
+// Definiamo almeno 3 pagine: una homepage, una pagina “chi siamo” e una pagina con la lista dei post
+// Implementiamo una navbar in comune a tutte le pagine per poter navigare tra loro
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 export default function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={Home} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,187 @@
+export default function Home() {
+  return (
+    <>
+      <header>
+        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+          <div class="container">
+            <a class="navbar-brand" href="#">
+              Home
+            </a>
+            <button
+              class="navbar-toggler d-lg-none"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapsibleNavId"
+              aria-controls="collapsibleNavId"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavId">
+              <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" href="#" aria-current="page">
+                    About us
+                    <span class="visually-hidden">(current)</span>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    Posts
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <div class="p-5 mb-4 bg-light rounded-3">
+          <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">Custom jumbotron</h1>
+            <p class="col-md-8 fs-4">
+              Using a series of utilities, you can create this jumbotron, just
+              like the one in previous versions of Bootstrap. Check out the
+              examples below for how you can remix and restyle it to your
+              liking.
+            </p>
+            <button class="btn btn-primary btn-lg" type="button">
+              Example button
+            </button>
+          </div>
+        </div>
+        <div className="container">
+          <main class="px-3">
+            <h1>Cover your page.</h1>
+            <p class="lead">
+              Cover is a one-page template for building simple and beautiful
+              home pages. Download, edit the text, and add your own fullscreen
+              background photo to make it your own.
+            </p>
+            <p class="lead">
+              <a
+                href="#"
+                class="btn btn-lg btn-light fw-bold border-white bg-white"
+              >
+                Learn more
+              </a>
+            </p>
+          </main>
+        </div>
+      </main>
+      <footer>
+        <div class="container">
+          <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+            <div class="col mb-3">
+              <a
+                href="/"
+                class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none"
+              >
+                <svg class="bi me-2" width="40" height="32">
+                  <use xlink:href="#bootstrap"></use>
+                </svg>
+              </a>
+              <p class="text-body-secondary">© 2024</p>
+            </div>
+
+            <div class="col mb-3"></div>
+
+            <div class="col mb-3">
+              <h5>Section</h5>
+              <ul class="nav flex-column">
+                <li class="nav-item mb-2">
+                  <a href="#" class="nav-link p-0 text-body-secondary">
+                    Home
+                  </a>
+                </li>
+                <li class="nav-item mb-2">
+                  <a href="#" class="nav-link p-0 text-body-secondary">
+                    Features
+                  </a>
+                </li>
+                <li class="nav-item mb-2">
+                  <a href="#" class="nav-link p-0 text-body-secondary">
+                    Pricing
+                  </a>
+                </li>
+                <li class="nav-item mb-2">
+                  <a href="#" class="nav-link p-0 text-body-secondary">
+                    FAQs
+                  </a>
+                </li>
+                <li class="nav-item mb-2">
+                  <a href="#" class="nav-link p-0 text-body-secondary">
+                    About
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="col mb-3">
+              <h5>Section</h5>
+              <ul class="nav flex-column">
+                <li class="nav-item mb-2">
+                  <a href="#" class="nav-link p-0 text-body-secondary">
+                    Home
+                  </a>
+                </li>
+                <li class="nav-item mb-2">
+                  <a href="#" class="nav-link p-0 text-body-secondary">
+                    Features
+                  </a>
+                </li>
+                <li class="nav-item mb-2">
+                  <a href="#" class="nav-link p-0 text-body-secondary">
+                    Pricing
+                  </a>
+                </li>
+                <li class="nav-item mb-2">
+                  <a href="#" class="nav-link p-0 text-body-secondary">
+                    FAQs
+                  </a>
+                </li>
+                <li class="nav-item mb-2">
+                  <a href="#" class="nav-link p-0 text-body-secondary">
+                    About
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="col mb-3">
+              <h5>Section</h5>
+              <ul class="nav flex-column">
+                <li class="nav-item mb-2">
+                  <a href="#" class="nav-link p-0 text-body-secondary">
+                    Home
+                  </a>
+                </li>
+                <li class="nav-item mb-2">
+                  <a href="#" class="nav-link p-0 text-body-secondary">
+                    Features
+                  </a>
+                </li>
+                <li class="nav-item mb-2">
+                  <a href="#" class="nav-link p-0 text-body-secondary">
+                    Pricing
+                  </a>
+                </li>
+                <li class="nav-item mb-2">
+                  <a href="#" class="nav-link p-0 text-body-secondary">
+                    FAQs
+                  </a>
+                </li>
+                <li class="nav-item mb-2">
+                  <a href="#" class="nav-link p-0 text-body-secondary">
+                    About
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </footer>
+        </div>
+      </footer>
+    </>
+  );
+}
