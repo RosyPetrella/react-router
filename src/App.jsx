5879bfd7e416468014fa0a43f928218b72f00928
RosyPetrella <petrellamr@gmail.com>
2025-04-01 15:22:49 +0200

    Add Info and Posts pages with navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@
 // Implementiamo una navbar in comune a tutte le pagine per poter navigare tra loro
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Info from "./pages/Info";
+import Posts from "./pages/Posts";
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" Component={Home} />
+          <Route path="/Info" Component={Info} />
+          <Route path="/Posts" Component={Posts} />
         </Routes>
       </BrowserRouter>
     </>
